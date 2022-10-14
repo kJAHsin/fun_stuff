b5474f284076d756ad516c96950deef88bc8fdd9
kJAHsin <106404582+kJAHsin@users.noreply.github.com>
2022-10-14 15:47:47 +0200

    Add files via upload

---
+++ anim_landing_page/script.js
@@ -0,0 +1,24 @@
+
+
+const ACTIVECLASS = 'active';
+const IMAGES =  document.querySelectorAll('.slider');
+
+IMAGES[0].classList.add(ACTIVECLASS)
+
+function removeActiveClass() {
+    const elm = document.querySelector(`.${ACTIVECLASS}`)
+    if (elm) {
+        elm.classList.remove(ACTIVECLASS);
+    }
+}
+
+function addClass($event) {
+    $event.stopPropagation();
+    removeActiveClass();
+    const target = $event.currentTarget;
+    target.classList.add(ACTIVECLASS);
+}
+
+IMAGES.forEach(image =>  {
+    image.addEventListener('click', addClass);
+});
